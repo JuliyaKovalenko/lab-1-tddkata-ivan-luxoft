019c17e600c8c1b4811ac5e8662ef2c14a981e39
JuliyaKovalenko <58985551+JuliyaKovalenko@users.noreply.github.com>
2020-12-21 12:13:09 +0200

    refactoring 1)logic of customers actually delimeters dont working - not done at me 2) try{ arrayOfInteger = new ArgumentSplitter(inputString).Arguments;} could be without try catch - but there is a lot logic to to do охранное условие

---
+++ refactorTddCata2017/refactorTddCata2017/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("refactorTddCata2017")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("refactorTddCata2017")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8c8d601-6ab9-4b29-9a96-73205a022bc9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
